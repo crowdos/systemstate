d0b5db5e0df8055e6d96fe51db3ff7f468f65b1d
Mohammed Sameer <msameer@foolab.org>
2015-12-13 05:30:40 +0200

    Added a test plugin

---
+++ plugins/test.cpp
@@ -0,0 +1,36 @@
+#include "stateplugin.h"
+#include <iostream>
+
+using namespace systemstate;
+
+class TestPlugin : public Plugin {
+  void init(DirNode *root);
+  bool start(const FileNode *node);
+  void stop(const FileNode *node);
+  ssize_t size(const FileNode *node);
+  bool read(const FileNode *node, std::string& data);
+};
+
+void TestPlugin::init(DirNode *root) {
+  std::cerr << "init" << std::endl;
+  root->appendDir("Test")->appendFile("test", this);
+}
+
+bool TestPlugin::start(const FileNode *node) {
+  return true;
+}
+
+void TestPlugin::stop(const FileNode *node) {
+
+}
+
+ssize_t TestPlugin::size(const FileNode *node) {
+  return 0;
+}
+
+bool TestPlugin::read(const FileNode *node, std::string& data) {
+  data = "t";
+  return true;
+}
+
+REGISTER_STATE_PLUGIN(TestPlugin);
