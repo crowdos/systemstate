d79eb6694933ab9ec4c9b56e10a94e871083fa5a
Mohammed Sameer <msameer@foolab.org>
2016-02-02 18:47:46 +0200

    Use boost::asio::signal_set instead of our home grown signalfd()

--- src/main.cpp
+++ src/main.cpp
@@ -4,60 +4,6 @@
 #include "server.h"
 #include "utils.h"
 #include "plugindb.h"
-#include <sys/signalfd.h>
-
-class SignalHandler {
-public:
-  SignalHandler(Server& server) :
-    m_server(server),
-    m_fd(-1),
-    m_desc(boost::asio::posix::stream_descriptor(m_server.service())) {
-
-    sigset_t mask;
-    sigemptyset(&mask);
-    sigaddset(&mask, SIGTERM);
-    sigaddset(&mask, SIGQUIT);
-
-    if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1) {
-      throw std::logic_error(std::strerror(errno));
-    }
-
-    m_fd = signalfd(-1, &mask, 0);
-    if (m_fd == -1) {
-      throw std::logic_error(std::strerror(errno));
-    }
-
-    m_desc.assign(m_fd);
-  }
-
-  ~SignalHandler() {
-    m_desc.cancel();
-    m_desc.close();
-  }
-
-  void start() {
-    m_desc.async_read_some(boost::asio::null_buffers(),
-			   boost::bind(&SignalHandler::read, this));
-
-  }
-
-private:
-  void read() {
-    struct signalfd_siginfo fdsi;
-    memset(&fdsi, 0, sizeof(fdsi));
-    ssize_t s = ::read(m_fd, &fdsi, sizeof(struct signalfd_siginfo));
-    if (s != sizeof(struct signalfd_siginfo)){
-      start();
-      return;
-    }
-
-    m_server.shutdown();
-  }
-
-  Server& m_server;
-  boost::asio::posix::stream_descriptor m_desc;
-  int m_fd;
-};
 
 int
 main(int argc, char *argv[]) {
@@ -77,8 +23,15 @@ main(int argc, char *argv[]) {
     ::unlink(path.c_str());
     Server server(path, loader);
 
-    SignalHandler handler(server);
-    handler.start();
+    boost::asio::signal_set signals(server.service(), SIGQUIT, SIGINT, SIGTERM);
+    signals.async_wait([&server](const boost::system::error_code& error,
+				 int signal_number) {
+			 if (!error) {
+			   std::cerr << "signal " << signal_number << ". quitting..." << std::endl;
+			   server.shutdown();
+			 }
+		       });
+
     server.start();
     return server.loop();
   } catch (std::exception& ex) {
